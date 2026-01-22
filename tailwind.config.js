02f4d37db266dd3046a00b0340159454b01f2419
Jake Spievak <jakespvk@gmail.com>
2026-01-21 19:10:53 -0800

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-  daisyui: {
-	  themes: ["light"],
-  },
+    content: ["./index.html"],
+    theme: {
+        fontSize: {
+            sm: '0.8rem',
+            base: '1.2rem',
+            xl: '1.35rem',
+            '2xl': '1.563rem',
+            '3xl': '1.953rem',
+            '4xl': '2.441rem',
+            '5xl': '3.052rem',
+            '6xl': '3.75rem',
+            '7xl': '4.5rem',
+            '8xl': '6rem',
+            '9xl': '8rem',
+        },
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: ["light"],
+    },
 }
